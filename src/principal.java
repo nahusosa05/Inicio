8f3b676a7df488411626b06fa6ef93346cee2c8f
Nahuel <nasosa@frba.utn.edu.ar>
2024-09-09 01:32:24 -0300

    branch nico

--- src/principal.java
+++ src/principal.java
@@ -44,7 +44,7 @@ public class principal
 
         int num1,num2,resultado;
         num1 = 10;
-        num2 = 0;
+        num2 = 20;
 
         // los condicionales if y else funcionan igual que en c++
         if(num2==0)
@@ -100,5 +100,13 @@ public class principal
             System.out.println(nombreDia);
         }
         System.out.println("chau mundo");
+        System.out.println("fernandito");
+
+        int edad = 20
+        for(int f=0; f<nombreDia.length() ;f++)
+        {
+            System.out.println("Tu edad es: " + edad);
+        }
+
 	}
 }
