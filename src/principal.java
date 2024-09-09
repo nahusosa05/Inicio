4932cdc1663c23e89485c4be43e18e7fa5eece76
Nahuel <nasosa@frba.utn.edu.ar>
2024-09-09 01:45:32 -0300

    main cambiado

--- src/principal.java
+++ src/principal.java
@@ -1,34 +1,7 @@
-class Persona 
-{
-    String nombre;
-    int edad;
-    String direccion;
-
-    public Persona(String n,String d ,int e) 
-    {
-        this.nombre = n;
-        this.direccion = d;
-        this.edad = e;
-    }
-
-    void saludar(Persona p) 
-    {
-        System.out.println("Hola " + p.nombre + " - Tu edad: " +  p.edad + " y tu direccion es: " + p.direccion );
-    }
-}
-
 public class principal
 {
 	public static void main(String[] args)
 	{
-		Persona persona1 = new Persona("Nahuel","Av. Castañares 4273",20);
-		persona1.saludar(persona1);
-
-        String h = "hola";
-        for(int i =0; i<h.length();i++)
-        {
-            char c = h.charAt(i);
-            System.out.println(c);
-        }
+		System.out.println("Hola mundo");
 	}
 }
